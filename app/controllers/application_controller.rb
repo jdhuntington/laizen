6209ae3b13e90060259ac6934c7300756f7b0446
JD Huntington <jd@aboutus.org>
2010-05-07 11:02:48 -0700

    Require authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@
 
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
+
+  before_filter :authenticate
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
 end
